ef0c0c67e84049b718a60cfb37551d94553c217c
unknown <nishanth1399@gmail.com>
2021-09-16 19:33:40 +0530

    Initial Commit

---
+++ movie/movie/Data/movieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using movie.Models;
+
+namespace movie.Data
+{
+    public class movieContext : DbContext
+    {
+        public movieContext (DbContextOptions<movieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<movie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ movie/movie/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
